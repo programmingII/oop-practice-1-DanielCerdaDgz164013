824b74d0a88f997a485e8fdddbbe92e40b86ec24
DanielCerdaDgz164013 <47226519+DanielCerdaDgz164013@users.noreply.github.com>
2019-02-06 20:41:56 -0700

    Add files via upload

---
+++ Ejercicio3.java
@@ -0,0 +1,17 @@
+public class Ejercicio3{
+public static void main (String[] args){
+
+ int num1 = 50;
+ int num2 = 3;
+ float result;
+
+ System.out.println("Division de 2 numeros");
+ 
+ result = num1 / num2;
+
+ System.out.println("El resultado de la resta es: " + result);
+
+
+ }
+
+}
