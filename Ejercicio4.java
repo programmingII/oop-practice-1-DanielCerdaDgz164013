1f828b9f97f69be10ae3e9b17fef69d68acf1171
DanielCerdaDgz164013 <47226519+DanielCerdaDgz164013@users.noreply.github.com>
2019-02-07 12:14:09 -0700

    Add files via upload

---
+++ Ejercicio4.java
@@ -0,0 +1,18 @@
+/*
+Daniel Cerda Dominguez 164013
+12:05
+*/
+
+public class Ejercicio4{
+public static void main(String[] args){
+
+System.out.println("Operaciones");
+
+System.out.println(-2+4*3);
+System.out.println(5+15/2);
+System.out.println((16-6)/4);
+System.out.println(50+2*2/2);
+
+ }
+
+}
