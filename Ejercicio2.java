f7799bbcbe1f4317de08ece18a894fb98e3eb98a
DanielCerdaDgz164013 <47226519+DanielCerdaDgz164013@users.noreply.github.com>
2019-02-06 20:23:09 -0700

    Add files via upload

---
+++ Ejercicio2.java
@@ -0,0 +1,17 @@
+public class Ejercicio2{
+
+ public static void main(String[] args){
+ System.out.println("Suma de 2 numeros");
+
+  int num1 = 14; 
+  int num2 = 69;
+  int result;
+
+  result = num1 + num2;
+
+  System.out.println("El resultado es: " + result);
+
+ }
+
+
+}
