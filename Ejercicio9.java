df299c273c289fefecbc3775dbd0212808d7bf8e
DanielCerdaDgz164013 <47226519+DanielCerdaDgz164013@users.noreply.github.com>
2019-02-12 12:50:55 -0700

    Add files via upload

---
+++ Ejercicio9.java
@@ -0,0 +1,13 @@
+/*
+Daniel Cerda Dominguez 164013
+12:45 - 12:49
+12 feb
+*/
+
+public class Ejercicio9{
+public static void main(String[] args){
+
+System.out.println((25.5*4.5-2.5*6.5)/(20-16));
+
+ }
+}
