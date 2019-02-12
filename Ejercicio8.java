9246f7a1d9cc1e670d0381e5eebc8bab117c2491
DanielCerdaDgz164013 <47226519+DanielCerdaDgz164013@users.noreply.github.com>
2019-02-12 12:44:42 -0700

    Add files via upload

---
+++ Ejercicio8.java
@@ -0,0 +1,17 @@
+/*
+Daniel Cerda Dominguez 164013
+12:32 - 12:43
+12 feb
+*/
+
+public class Ejercicio8{
+public static void main(String[] args){
+
+System.out.println("   J     A       V     V     A            ");
+System.out.println("   J    A A       V   V     A A          ");
+System.out.println("J  J   AAAAAA      V V     AAAAA          ");
+System.out.println(" JJ    A     A      V     A     A         ");
+
+
+ }
+}
