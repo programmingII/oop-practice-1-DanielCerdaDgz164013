7980b663acad59f300fbc136aa6644e912ba7852
DanielCerdaDgz164013 <47226519+DanielCerdaDgz164013@users.noreply.github.com>
2019-02-12 12:57:51 -0700

    Add files via upload

---
+++ Ejercicio10.java
@@ -0,0 +1,13 @@
+/*
+Daniel Cerda Dominguez 164013
+12:51 - 
+12 febrero
+*/
+
+public class Ejercicio10{
+public static void main(String[] args){
+
+System.out.println(4*(2.0/2)+(1.0/4)+(4.0/2)-(2.015));
+
+ }
+}
