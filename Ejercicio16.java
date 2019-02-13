949068ef0443292b405470f677684e6358cca530
DanielCerdaDgz164013 <47226519+DanielCerdaDgz164013@users.noreply.github.com>
2019-02-13 16:19:01 -0700

    Add files via upload

---
+++ Ejercicio16.java
@@ -0,0 +1,18 @@
+/*
+Daniel Cerda Dominguez 164013
+Inicio: 4:13
+Termino: 4:16
+Dia: 13 febrero
+*/
+
+public class Ejercicio16{
+public static void main(String[] args){
+
+System.out.println(" +'''''+");
+System.out.println("[| o o |]");
+System.out.println(" |  ^  |");
+System.out.println(" | '-' |");
+System.out.println(" +-----+");
+
+ }
+}
