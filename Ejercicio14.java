1fc6e65232fb834f461401fa396b878f1b1e4295
DanielCerdaDgz164013 <47226519+DanielCerdaDgz164013@users.noreply.github.com>
2019-02-13 15:24:56 -0700

    Add files via upload

---
+++ Ejercicio14.java
@@ -0,0 +1,28 @@
+/*
+Daniel Cerda Dominguez 164013
+Inicio: 3:18
+Termino: 3:24
+Dia: 13 febrero
+*/
+
+public class Ejercicio14{
+public static void main(String[] args){
+
+System.out.println("*  *  *  *  *  *  ===================");
+System.out.println("  *  *  *  *  *   ===================");
+System.out.println("*  *  *  *  *  *  ===================");
+System.out.println("  *  *  *  *  *   ===================");
+System.out.println("*  *  *  *  *  *  ===================");
+System.out.println("  *  *  *  *  *   ===================");
+System.out.println("*  *  *  *  *  *  ===================");
+System.out.println("  *  *  *  *  *   ===================");
+System.out.println("*  *  *  *  *  *  ===================");
+System.out.println("=====================================");
+System.out.println("=====================================");
+System.out.println("=====================================");
+System.out.println("=====================================");
+System.out.println("=====================================");
+System.out.println("=====================================");
+
+ }
+}
