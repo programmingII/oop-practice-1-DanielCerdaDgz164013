da9d79af636e520b24c080b79e0cf32ac8f4e410
DanielCerdaDgz164013 <47226519+DanielCerdaDgz164013@users.noreply.github.com>
2019-02-13 16:12:34 -0700

    Add files via upload

---
+++ Ejercicio15.java
@@ -0,0 +1,23 @@
+/*
+Daniel Cerda Dominguez 164013
+Inicio: 3:45
+Termino: 4:10
+Dia: 13 feb
+*/
+
+public class Ejercicio15{
+public static void main(String[] args){
+
+int a, b, c;
+a = 23;
+b = 62;
+
+System.out.println("Primero es: "+a+" " + + b);
+c = a;
+a = b;
+b = c;
+
+System.out.println("Luego es: "+a+ " "+ + b);
+
+ }
+}
